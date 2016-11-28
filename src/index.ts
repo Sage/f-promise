cdf05d9dc4f701ed48debde3849f605664066492
Bruno Jouhier <bjouhier@gmail.com>
2016-11-28 23:07:37 +0100

    added typescript dependency

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { _ } from 'streamline-runtime';
 
 export function wait<T>(promise: Promise<T>): T {
     const streamlined = ((_: _) => promise.then(_, _))
-    return streamlined['fiberized-0'].apply(this, [true]);
+    return (streamlined as any)['fiberized-0'].call(null, true);
 }
 
 export function run<T>(fn: () => T): Promise<T> {
